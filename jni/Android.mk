8320f46bad1c38e69aae50bfde41e079f4be9e30
SHuiTU68 <2679614794@qq.com>
2026-02-08 22:55:30 +0800

    更新 Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,15 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := sys_service_monitor  # 伪装成系统监控服务
+LOCAL_MODULE := sys_log_monitor  # 伪装名
 LOCAL_SRC_FILES := main.cpp
-
-# 极致轻量化与隐蔽性参数：
-# -fvisibility=hidden: 隐藏所有符号名
-# -fno-rtti/exceptions: 去掉 C++ 特征，减小体积
-# -Wl,--gc-sections: 剔除无用代码
-# -Os: 极致大小优化
+# 去掉标准库链接，只保留最基础的系统调用，极致轻量
+LOCAL_LDFLAGS += -Wl,--gc-sections -Wl,--strip-all -nostdlib++ 
 LOCAL_CPPFLAGS += -fvisibility=hidden -fno-rtti -fno-exceptions -Os
-LOCAL_LDFLAGS += -Wl,--exclude-libs,ALL -Wl,--gc-sections -Wl,--strip-all
 
 include $(BUILD_SHARED_LIBRARY)
