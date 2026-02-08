442f405fc67ad04e303d3356b9ff0791b8bd2e0d
SHuiTU68 <2679614794@qq.com>
2026-02-08 08:30:38 +0800

    更新 mian.cpp

--- jni/mian.cpp
+++ jni/mian.cpp
@@ -5,6 +5,7 @@
 #include <sched.h>
 #include "zygisk.hpp"
 
+// 显式指定命名空间，修复 AppSpecializeArgs 报错
 using zygisk::Api;
 using zygisk::AppSpecializeArgs;
 
@@ -13,13 +14,13 @@ public:
     void onLoad(Api* api, JNIEnv* env) override {}
 
     void preAppSpecialize(AppSpecializeArgs* args) override {
-        // [加强卸载] 使用 MNT_DETACH 彻底隐藏 su，解决卸载失效问题
+        // [隐蔽性增强] 隔离命名空间并强制卸载
         unshare(CLONE_NEWNS); 
         umount2("/system/bin/su", MNT_DETACH);
         umount2("/dev/ksu", MNT_DETACH);
         umount2("/proc/ksu", MNT_DETACH);
 
-        // [内核态联动] 模拟 KernelNoSU 0.0.6 禁用 sucompat
+        // [权限补丁] 联动内核禁用 sucompat
         int fd = open("/proc/sys/kernel/sucompat_enabled", O_WRONLY);
         if (fd != -1) {
             write(fd, "0", 1);
