476045914ef1747f4870a212926822cfb5843120
SHuiTU68 <2679614794@qq.com>
2026-02-08 22:54:19 +0800

    更新 Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,9 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := custom_rezygisk
+LOCAL_MODULE := sys_service_monitor  # 伪装成系统监控服务
 LOCAL_SRC_FILES := main.cpp
-LOCAL_LDLIBS    := -llog
-# 强制开启 C++17 且关闭不必要的符号导出以增强隐蔽性
-LOCAL_CPPFLAGS  := -std=c++17 -fvisibility=hidden
+
+# 极致轻量化与隐蔽性参数：
+# -fvisibility=hidden: 隐藏所有符号名
+# -fno-rtti/exceptions: 去掉 C++ 特征，减小体积
+# -Wl,--gc-sections: 剔除无用代码
+# -Os: 极致大小优化
+LOCAL_CPPFLAGS += -fvisibility=hidden -fno-rtti -fno-exceptions -Os
+LOCAL_LDFLAGS += -Wl,--exclude-libs,ALL -Wl,--gc-sections -Wl,--strip-all
+
 include $(BUILD_SHARED_LIBRARY)
