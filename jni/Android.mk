97d22abc60eff82b2e30182dc7125cc3331be7e9
SHuiTU68 <2679614794@qq.com>
2026-02-09 00:22:26 +0800

    更新 Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := sys_stealth_bridge
 LOCAL_SRC_FILES := main.cpp
+# 确保 C++17 支持和必要的链接
 LOCAL_CPPFLAGS := -std=c++17 -fno-rtti -fno-exceptions
 LOCAL_LDLIBS := -llog
 include $(BUILD_SHARED_LIBRARY)
