10a025a1e8a8b3a8ff92f81008413120a78aca39
SHuiTU68 <2679614794@qq.com>
2026-02-08 08:42:36 +0800

    更新 Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,7 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE    := System_Log_Helper
+
+LOCAL_MODULE    := custom_rezygisk
 LOCAL_SRC_FILES := main.cpp
 LOCAL_LDLIBS    := -llog
-LOCAL_CPPFLAGS  := -std=c++17
+# 强制开启 C++17 且关闭不必要的符号导出以增强隐蔽性
+LOCAL_CPPFLAGS  := -std=c++17 -fvisibility=hidden
 include $(BUILD_SHARED_LIBRARY)
